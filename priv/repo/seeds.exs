55838f51b6843b7845faf14d26699fd5ed4cc0e2
newton <newton.sarker@fortumo.com>
2021-10-17 00:00:30 +0300

    updated seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,7 +12,9 @@
 
 alias Takso.{Repo, Accounts.User}
 
-[%{name: "Fred Flintstone", username: "fred", password: "parool"},
-  %{name: "Barney Rubble", username: "barney", password: "parool"}]
+[
+  %{name: "Fred Flintstone", username: "fred", password: "parool"},
+  %{name: "Barney Rubble", username: "barney", password: "parool"}
+]
 |> Enum.map(fn user_data -> User.changeset(%User{}, user_data) end)
 |> Enum.each(fn changeset -> Repo.insert!(changeset) end)
